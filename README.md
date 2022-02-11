b6926f5fa88f337278f5cc94843f1c162148a88b
fiskhandlarn <oskar@joelson.org>
2022-02-11 21:20:41 +0100

    Add npm packages, configure vite, add skel html/css/js, add favicon

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # birkan
 Bacon or beer can or Birkan‽
 
+## Install
+
+```bash
+npm install
+```
+
+## Build assets
+
+```bash
+$ npm run build
+```
+
+## Local development
+
+Serve [`public/`](./public/) with [http-server](https://www.npmjs.com/package/http-server):
+
+```bash
+$ npm run serve
+```
+
 ## License
 [GNU Affero General Public License v3.0+](http://www.gnu.org/licenses/agpl-3.0.html "GNU GPL 3.0")
 
---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import viteStylelint from '@amatlash/vite-plugin-stylelint';
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
+
+export default ({ command }) => ({
+  base: command === 'serve' ? '/' : '/build/',
+  publicDir: 'resources/static',
+  build: {
+    manifest: false,
+    outDir: `public/assets`,
+    assetsDir: '',
+    minify: (command === 'serve' || command === '') ? false : 'terser',
+    rollupOptions: {
+      input: [
+        'resources/scripts/app.js',
+        'resources/styles/app.scss',
+      ],
+      output: {
+        entryFileNames: `[name]_0.1.js`,
+        chunkFileNames: `[name]_0.1.js`,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'app.css') {
+            return 'app_0.1.css';
+          }
+          return assetInfo.name;
+        },
+        manualChunks: null,
+      },
+    },
+  },
+  plugins: [
+    viteStylelint(),
+    ViteFaviconsPlugin({
+      logo: 'resources/static/images/favicons/favicon.png',
+      favicons: {
+        appName: 'Birkan',
+        background: '#f60',
+        theme_color: '#1d1d1d',
+        path: `/themes/assets/`,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: false,
+          coast: true,
+          favicons: true,
+          firefox: false,
+          windows: true,
+          yandex: true,
+        },
+      },
+    }),
+  ],
+});
